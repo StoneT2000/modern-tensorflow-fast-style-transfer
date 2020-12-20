6ba95787479761c476c4d9277bbc6448d7b3af11
Stone Tao <stonezt2019@gmail.com>
2020-12-20 15:42:56 -0800

    cleanp

--- src/test.py
+++
@@ -1,9 +0,0 @@
-from loss import loss
-from model import get_vgg_model
-from train import get_img, parse_image
-import cv2
-
-img = get_img("images/content/chicago.jpg")
-img = parse_image(img)
-print(img.shape)
-cv2.imwrite("test.png",cv2.cvtColor(img.numpy(), cv2.COLOR_RGB2BGR))
